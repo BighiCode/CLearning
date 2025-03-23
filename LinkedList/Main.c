f1f84115b39ec522b9162eb4b02c32bdd8c192e8
BighiCode <gabrielbighi27@gmail.com>
2025-03-23 15:29:32 -0300

    modified:   LinkedList/Main.c 	modified:   LinkedList/SimpleLinkedList.c 	modified:   LinkedList/SimpleLinkedList.h

--- LinkedList/Main.c
+++ LinkedList/Main.c
@@ -1,31 +1,7 @@
 #include "SimpleLinkedList.h"
 
-typedef struct teste{
-    int x;
-    double y;
-    int z;
-}Teste;
+int main(){
 
-
-int main(void){
-
-    int *x = malloc(sizeof(int));
-    double *y = malloc(sizeof(double));
-    int *z = malloc(sizeof(int));
-    Teste *t = malloc(sizeof(Teste));
-    *t = (Teste){45, 57.2, 42};
-    *x = 45;
-    *y = 57.2;
-    *z = 42;
-
-    Node* head = NULL;
-
-    addNode(&head, x);
-    addNode(&head, y);
-    addNode(&head, z);
-    addNode(&head, t);
-
-    printList(head);
 
     return 0;
 }
--- LinkedList/SimpleLinkedList.h
+++ LinkedList/SimpleLinkedList.h
@@ -1,14 +1,10 @@
+#ifndef SIMPLELINKEDLIST_H
+#define SIMPLELINKEDLIST_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
 
-typedef struct node
-{
-    void* estrutura;
-    struct node *next;
-}Node;
 
-Node* createNode(void* estrutura);
-void addNode(Node** head, void* estrutura);
-void printList(Node* head);
-void printNode(Node* node);
+
+#endif
