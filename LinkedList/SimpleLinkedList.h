2448f430d5b5fdc14cfbcf72064231f48cb94110
BighiCode <gabrielbighi27@gmail.com>
2025-03-23 16:03:49 -0300

    modified:   LinkedList/SimpleLinkedList.h 	modified:   Main.c

--- LinkedList/SimpleLinkedList.h
+++ LinkedList/SimpleLinkedList.h
@@ -4,7 +4,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct NODE
+{
+    int value;
+    struct NODE* prox;
+} Lnode;
 
 
+Lnode *CreateList();
+
+int IsEmpty(Lnode *list);
+
+Lnode *CreateNode(int value);
+
+Lnode SearchElementNode(Lnode *list, int value);
+
+Lnode SearchElementNodeByPosition(Lnode *list, int position);
+
+Lnode InsertInPositionX(Lnode **list, Lnode *newNode, int postion);
+
+Lnode *RemovePosition(Lnode **list, int position);
+
+void PrintList(Lnode *list);
 
 #endif
--- Main.c
+++ Main.c
@@ -1,7 +1,10 @@
 #include "LinkedList/SimpleLinkedList.h"
+#include "Tree/Tree.h"
+#include "Pile/Pile.h"
 
 int main(){
 
+    printf("Hello World\n");
 
     return 0;
 }
